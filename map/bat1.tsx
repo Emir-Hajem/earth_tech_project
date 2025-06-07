9e07f6b74b71f5d7d0751c55c600c2afd5b8ed05
Emir-Hajem <emir.hajem@efrei.net>
2025-06-07 21:03:47 +0200

    Add files via upload

---
+++ map/bat1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="bat1" tilewidth="32" tileheight="32" tilecount="224" columns="16">
+ <image source="tileset1.png" width="512" height="448"/>
+</tileset>
